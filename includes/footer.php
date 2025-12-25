17322699b30200df34a31bc70582ab49ffd88ff2
Bipin Chapai <96807663+biptech@users.noreply.github.com>
2025-12-25 15:45:34 +0545

    Created footer section with same colour theme

---
+++ includes/footer.php
@@ -0,0 +1,60 @@
+<footer class="site-footer">
+  <div class="container footer-grid">
+
+    <!-- ABOUT -->
+    <div class="footer-box">
+      <div class="footer-box-logo">
+        <a href="/TakeYourSeat/index.php" class="logo">
+          <img src="/TakeYourSeat/assets/images/TakeyourSeat_Logo-Color.png" alt="Take Your Seat Logo">
+          <div class="footer-lgt">
+            <p>"LET'S GO TOGETHER"</p>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <!-- QUICK LINKS -->
+    <div class="footer-box">
+      <h3>Quick Links</h3>
+      <ul>
+        <!-- <li><a href="index.php">Home</a></li> -->
+        <li><a href="tours.php">Tour Packages</a></li>
+        <li><a href="services.php">Our Services</a></li>
+        <li><a href="gallery.php">Gallery</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="about.php">About Us</a></li>
+      </ul>
+    </div>
+
+    <!-- TOURS -->
+    <div class="footer-box">
+      <h3>Popular Tours</h3>
+      <ul>
+        <li><a href="#">Nepal Cultural Tour</a></li>
+        <li><a href="#">Kathmandu & Pokhara Tour</a></li>
+        <li><a href="#">Dubai Holiday Package</a></li>
+        <li><a href="#">Thailand Tour</a></li>
+      </ul>
+    </div>
+
+
+
+
+  </div>
+
+  <!-- COPYRIGHT -->
+  <div class="footer-bottom">
+    <p>© <?php echo date("Y"); ?> Take Your Seat. All Rights Reserved.</p>
+  </div>
+</footer>
+
+<!-- LIGHTBOX -->
+<div id="lightbox">
+  <span id="closeLightbox">&times;</span>
+  <img id="lightboxImg">
+</div>
+
+
+</body>
+</html>
+  
