53ae28352ffb62e5b789a9e21bb3d51109d5c53f
KooSL <142590197+KooSL@users.noreply.github.com>
2025-12-28 19:16:18 +0545

    Add HTML structure to header.php for admin panel

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,1 +1,28 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>TYS Admin Panel</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- ADMIN CSS -->
+  <link rel="stylesheet" href="/TakeYourSeat/admin/assets/css/admin.css">
+</head>
+<body>
+
+<!-- TOP HEADER -->
+
+<header class="admin-header">
+  <div class="admin-header-inner">
+    <h2></h2>
+
+    <div class="admin-user">
+      <!-- <span>Admin</span> -->
+      <?php if (isset($_SESSION['admin'])) { ?>
+        <a href="logout.php">Logout</a>
+      <?php } ?>
+      
+    </div>
+  </div>
+</header>
