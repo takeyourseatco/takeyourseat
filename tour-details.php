af3b812445264cda0db4aaf0b4246d4fcee8c72a
Bipin Chapai <96807663+biptech@users.noreply.github.com>
2025-12-31 16:19:06 +0545

    Create tour details page layout with common includes

---
+++ tour-details.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="header-wrapper">
+    <?php include 'includes/topbar.php'; ?>
+    <?php include 'includes/navbar.php'; ?>
+</div>
+
+<section class="container tour-layout">
+
+    <div class="tour-content">
+        <!-- Tour content will be added here -->
+    </div>
+
+    <aside class="tour-sidebar">
+        <!-- Sidebar content will be added here -->
+    </aside>
+
+</section>
+
+<?php include 'includes/footer.php'; ?>
